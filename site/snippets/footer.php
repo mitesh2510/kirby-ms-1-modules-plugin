1c196c5725519559e2643f7d281e45aa82e3ea4c
Mitesh <cmilan@pistalix.com>
2017-12-25 12:22:35 +0530

    Htaccess

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,5 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/photoswipe.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.2/photoswipe-ui-default.min.js"></script>
-    
+    <?= \ka\kirby\PhotoSwipe::init(); ?>
   </body>
 </html>
